3baa984c4294fb8b71f2484e0846fb9a1477cfff
rgvieira <yami.hiei87@gmail.com>
2023-05-01 15:46:52 -0300

    Added axios and created the send reply funciton on ReceiveMessageWebhook

--- src/modules/whatsapp/useCases/receiveMessageWebhook/ReceiveMessageWebhookUseCase.ts
+++ src/modules/whatsapp/useCases/receiveMessageWebhook/ReceiveMessageWebhookUseCase.ts
@@ -1,13 +1,42 @@
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
 interface IRequest {
   phone_number_id: number;
   from: number;
   message: string;
 }
 
+interface IMessage {
+  messaging_product: string;
+  to: number;
+  text: {
+    body: string;
+  };
+}
+
 class ReceiveMessageWebhookUseCase {
 
   async execute({ phone_number_id, from, message }: IRequest): Promise<void> {
-    console.log(`New messagre from: ${from} - Content: ${message}`);
+
+    const token = process.env.WHATSAPP_TOKEN;
+
+    const url = `https://graph.facebook.com/v16.0/${phone_number_id}/messages?access_token=${token}`;
+
+    const data: IMessage = {
+      messaging_product: "whatsapp",
+      to: from,
+      text: { body: "Olá, tudo bem?" },
+    };
+
+    axios.post<IMessage, AxiosResponse<IMessage>>(url, data)
+      .then(response => {
+        console.log('Response:', response.data);
+      })
+      .catch((error: AxiosError) => {
+        console.error('Error:', error.response?.data ?? error.message);
+      });
+
+    console.log(`New messagre from: ${from} - Content: ${message} `);
   }
 }
 
